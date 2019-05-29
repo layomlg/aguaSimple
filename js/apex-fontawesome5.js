047e01f7759025df0b5de1a34eef856cc38684f5
layato <omarpelayo.cubo1221@gmail.com>
2019-05-29 16:20:11 -0500

    Inicio del repositorio agua simple

---
+++ js/apex-fontawesome5.js
@@ -0,0 +1,7 @@
+var FontAwesomeConfig = { autoReplaceSvg: 'nest' };
+
+(function(){
+	// using far (regular) as the default style
+	$('[class^="fa-"], [class*=" fa-"]').not(".fas, .far, .fal, .fab").addClass("far");
+})();
+//# sourceMappingURL=apex-fontawesome5.js.map
